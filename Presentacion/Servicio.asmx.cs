417695689fa3aa804e38a91f067c1fda63920c77
Nicolas Guido <guidonicolas25@gmail.com>
2019-11-07 19:13:02 -0300

    Web Server

---
+++ Presentacion/Servicio.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Negocio;
+using System.Data;
+
+namespace Presentacion
+{
+    /// <summary>
+    /// Descripción breve de Servicio
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Servicio : System.Web.Services.WebService
+    {
+        private TurnoNego turnoNego = new TurnoNego();
+
+        [WebMethod]
+        public DataTable ListadoTurnos()
+        {
+            return turnoNego.ListarTurno();
+        }
+
+    }
+}
